7dc4b552aa6f21162ae7b3892f80d67d2efd5bd5
Aryan <aryanamittiwari@gmail.com>
2025-04-08 22:51:27 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
 
-ReactDOM.render(
+// Create a root element
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render the app
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
